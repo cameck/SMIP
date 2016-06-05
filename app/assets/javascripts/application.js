0eff5b8f21b6efb00a4e2da19416619070b3a7a8
Cameron Eckelberry <cameck@gmail.com>
2016-06-05 12:43:45 -0400

    Added click to copy, using selection as it has cross-browser support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,4 +27,34 @@ $(document).ready(function(){
     }
   );
 
+  // Copy to Clipboard function
+  function SelectText(element) {
+      var doc = document;
+      var text = doc.getElementById(element);
+      var range;
+      var selection;
+
+      if (doc.body.createTextRange) {
+          range = document.body.createTextRange();
+          range.moveToElementText(text);
+          range.select();
+      } else if (window.getSelection) {
+          selection = window.getSelection();
+          range = document.createRange();
+          range.selectNodeContents(text);
+          selection.removeAllRanges();
+          selection.addRange(range);
+      }
+  }
+
+
+  $(document).on('click', '#url-holder', function(e) {
+    console.log("CLICKED!");
+    if (e.target.id === 'd_clip_button') {
+          SelectText('copy-link');
+          console.log("DONE!");
+        }
+  });
+
+
 });
