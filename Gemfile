ef7508b6d80944b3744833c2da969645bf6c5442
Cameron Eckelberry <cameck@gmail.com>
2018-04-21 16:23:39 -0400

    fix database configs... what was i thinking...

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
 
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'coffee-rails', '~> 4.1.0'
 gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
-gem 'pg'
+gem 'pg', '~> 0.20'
 gem 'puma'
 gem 'rails', '~> 4.2.7.1'
 gem 'responders'
--- db/schema.rb
+++ db/schema.rb
@@ -11,45 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519212446) do
+ActiveRecord::Schema.define(version: 20160503171233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "messages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "saved_search_items", force: :cascade do |t|
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.decimal  "sentiment_score", precision: 5, scale: 2
-    t.integer  "saved_search_id"
-  end
-
-  add_index "saved_search_items", ["saved_search_id"], name: "index_saved_search_items_on_saved_search_id", using: :btree
-
-  create_table "saved_searches", force: :cascade do |t|
-    t.string   "search"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sentiments", force: :cascade do |t|
-    t.string   "emoji"
-    t.decimal  "sentiment",  precision: 5, scale: 2
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-  end
-
-  create_table "twitter_bots", force: :cascade do |t|
-    t.string   "client"
-    t.string   "search"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "url_shortens", force: :cascade do |t|
     t.string   "original_url"
@@ -63,26 +28,4 @@ ActiveRecord::Schema.define(version: 20160519212446) do
     t.string "small_url"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "last_name"
-    t.string   "company_name"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  add_foreign_key "saved_search_items", "saved_searches"
 end
